9051f4dfd649c0e09b8e5f6a824e13c245d0fa1b
ani17 <ani17@github.com>
2017-05-10 17:33:02 +0530

    globe transition

---
+++ globe/README.md
@@ -0,0 +1,3 @@
+Globe Transition
+- This is basic example of Globe Transition using d3.js javascript library.
+- It uses custom json country data recieved from ajax to point to the respective location on the globe.
---
+++ globe/ajx.php
@@ -0,0 +1,22 @@
+<?php
+$info = array
+(
+	array
+	(
+		'id' => '4',
+		'name' => 'Afghanistan'
+	),
+	array
+	(
+		'id' => '76',
+		'name' => 'Brazil'
+	),
+	array
+	(
+		'id' => '276',
+		'name' => 'Germany'
+	)
+);
+
+echo json_encode($info);
+?>
